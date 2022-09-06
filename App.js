07cdec9f7ed937efc4e589d294e72935284710e7
Isaac Mendoza <isaac.mendozasa@gmail.com>
2022-09-06 00:57:02 -0400

    finish the project test of acme

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { useEffect, useState } from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
-export default function App() {
+import Auth from './src/components/Login/Auth'
+import Orders from './src/components/Orders/Orders'
+
+const Stack = createNativeStackNavigator()
+
+const App = () => {
+  const [isToken, setIsToken] = useState('')
+
+  useEffect(() => {
+    async function getToken() {
+      try {
+        const getUserToken = await AsyncStorage.getItem('token')
+        setIsToken(getUserToken)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    getToken()
+  })
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        {isToken !== null ? (
+          <Stack.Screen name="Pedidos" options={{ title: 'Pedidos' }}>
+            {(props) => <Orders {...props} setIsToken={setIsToken} />}
+          </Stack.Screen>
+        ) : (
+          <Stack.Screen name="Home" options={{ title: 'Home' }}>
+            {(props) => <Auth {...props} setIsToken={setIsToken} />}
+          </Stack.Screen>
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
